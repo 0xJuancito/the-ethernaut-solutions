c6403d349384a8cd898237e7b63b2e31be1bf177
juancito.eth <juancitoeth@gmail.com>
2022-08-26 16:56:00 -0300

    setup hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,44 +1,44 @@
-import '@nomicfoundation/hardhat-toolbox';
-import { config as dotenvConfig } from 'dotenv';
-import type { HardhatUserConfig } from 'hardhat/config';
-import { resolve } from 'path';
-import 'tsconfig-paths/register';
+import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import type { HardhatUserConfig } from "hardhat/config";
+import { resolve } from "path";
+import "tsconfig-paths/register";
 
 // import "./tasks";
 
-dotenvConfig({ path: resolve(__dirname, './.env') });
+dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 // Ensure that we have all the environment variables we need.
-const privateKey1: string = process.env.ACCOUNT_PRIVATE_KEY_1 || '';
-const privateKey2: string = process.env.ACCOUNT_PRIVATE_KEY_2 || '';
+const privateKey1: string = process.env.ACCOUNT_PRIVATE_KEY_1 || "";
+const privateKey2: string = process.env.ACCOUNT_PRIVATE_KEY_2 || "";
 if (!privateKey1 || !privateKey2) {
-  throw new Error('Please set your ACCOUNT_PRIVATE_KEY in a .env file');
+  throw new Error("Please set your ACCOUNT_PRIVATE_KEY in a .env file");
 }
 
 const alchemyApiKey: string | undefined = process.env.ALCHEMY_API_KEY;
 if (!alchemyApiKey) {
-  throw new Error('Please set your ALCHEMY_API_KEY in a .env file');
+  throw new Error("Please set your ALCHEMY_API_KEY in a .env file");
 }
 
 const config: HardhatUserConfig = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   etherscan: {
     apiKey: {
-      arbitrumOne: process.env.ARBISCAN_API_KEY || '',
-      avalanche: process.env.SNOWTRACE_API_KEY || '',
-      bsc: process.env.BSCSCAN_API_KEY || '',
-      mainnet: process.env.ETHERSCAN_API_KEY || '',
-      optimisticEthereum: process.env.OPTIMISM_API_KEY || '',
-      polygon: process.env.POLYGONSCAN_API_KEY || '',
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
-      rinkeby: process.env.ETHERSCAN_API_KEY || '',
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      avalanche: process.env.SNOWTRACE_API_KEY || "",
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+      rinkeby: process.env.ETHERSCAN_API_KEY || "",
     },
   },
   gasReporter: {
-    currency: 'USD',
+    currency: "USD",
     enabled: process.env.REPORT_GAS ? true : false,
     excludeContracts: [],
-    src: './contracts',
+    src: "./contracts",
   },
   networks: {
     hardhat: {
@@ -52,18 +52,18 @@ const config: HardhatUserConfig = {
     // rinkeby: getChainConfig("rinkeby"),
   },
   paths: {
-    artifacts: './artifacts',
-    cache: './cache',
-    sources: './contracts',
-    tests: './test',
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
   solidity: {
-    version: '0.6.0',
+    version: "0.6.2",
     settings: {
       metadata: {
         // Not including the metadata hash
         // https://github.com/paulrberg/hardhat-template/issues/31
-        bytecodeHash: 'none',
+        bytecodeHash: "none",
       },
       // Disable the optimizer when debugging
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
@@ -74,8 +74,8 @@ const config: HardhatUserConfig = {
     },
   },
   typechain: {
-    outDir: 'src/types',
-    target: 'ethers-v5',
+    outDir: "src/types",
+    target: "ethers-v5",
   },
 };
 
