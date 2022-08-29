ac34c1ee09b00ee5699fe8849b176f7f237f2263
juancito.eth <juancitoeth@gmail.com>
2022-08-29 00:50:28 -0300

    improve template

--- test/template.ts
+++ test/template.ts
@@ -1,17 +1,35 @@
+import { TransactionResponse } from "@ethersproject/providers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-const contractName = "";
-let tx;
+const CONTRACT_NAME = "";
 
-describe(contractName, () => {
-  it("Solves the challenge", async () => {
-    const factory = await ethers.getContractFactory(contractName);
-    const contract = await factory.deploy();
+describe(CONTRACT_NAME, () => {
+  let _owner: SignerWithAddress;
+  let attacker: SignerWithAddress;
+  let contract: Contract;
+  let tx: TransactionResponse;
+
+  beforeEach(async () => {
+    [_owner, attacker] = await ethers.getSigners();
+
+    const factory = await ethers.getContractFactory(CONTRACT_NAME);
+    contract = await factory.deploy();
     await contract.deployed();
 
-    tx = await contract.callme();
+    contract = contract.connect(attacker);
+  });
+
+  it("Solves the challenge", async () => {
+    tx = await contract.contribute({ value: 1 });
     await tx.wait();
+
+    tx = await attacker.sendTransaction({
+      to: contract.address,
+      value: 1,
+    });
 
     expect(await contract.isComplete()).to.be.true;
   });
